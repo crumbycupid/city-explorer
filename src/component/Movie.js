c9efa1a6253ec319474fccff2e91d1867166afca
Devon Terry <114770792+crumbycupid@users.noreply.github.com>
2022-12-02 02:37:45 -0500

    changes

--- src/component/Movie.js
+++ src/component/Movie.js
@@ -1,24 +1,41 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
 
 class Movie extends React.Component {
   render() {
-    let oneMovie = [];
-    let cityMovies = this.props.movies.map((movie,index) => (
-        <li key={index}>{movie.title} ({`${movie.year}`})</li>
-      ));
-      
-      oneMovie = cityMovies[0];
-  
-  return(
-  <>
-    <h2>{this.props.cityName} Movies</h2>
-    <ul>
-      {oneMovie}
-    </ul>
-  </>
+    let shortData = this.props.movieData;
 
-  );
+    let movieURL = '';
+
+    if(shortData.image_url === null){
+      movieURL = 'https://via.placeholder.com/500';
+    }else{
+      movieURL = `https://image.tmdb.org/t/p/w500/${shortData.image_url}`
+    }
+    
+    
+
+    
+
+    return(
+    <Col>
+      <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" alt="Movie Poster" src={movieURL} />
+      <Card.Body>
+        <Card.Title>{shortData.title}</Card.Title>
+        <Card.Text>
+          {shortData.description}
+        </Card.Text>
+        <p>❤️ {shortData.vote_average}</p>
+      </Card.Body>
+      </Card>
+    </Col>
+      
+
+    );
+  }
 }
-}
+
 
 export default Movie;
